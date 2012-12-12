2826b6bbb8991dce6096e246ac5022befe2ec361
Dennis Wegner <dennis@instant-thinking.de>
2012-12-12 06:25:53 +0100

    Added the content of Mac mini ~/bin

--- bin/rsync_macbook_to_nslu2.sh
+++ bin/rsync_macbook_to_nslu2.sh
@@ -4,10 +4,11 @@
 #NSLU2IP="myfamily.kicks-ass.org"
 
 #At family Sync
-#NSLU2IP="192.168.1.5"
+NSLU2IP="192.168.1.5"
 
 #At home Sync (dynamic, go watch the DHCP-Log...)
 NSLU2IP="192.168.5.191"
+
 
 echo "Backup auf die NSLU2" unter $NSLU2IP
 
