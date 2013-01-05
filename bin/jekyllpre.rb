11e6cec65ffc66931f9708255c5f88b9a2b477b7
Dennis Wegner <dennis@instant-thinking.de>
2013-01-05 21:19:45 +0100

    Jekyll Pre processor from Brett Terpstra added

---
+++ bin/jekyllpre.rb
@@ -0,0 +1,54 @@
+#!/usr/bin/ruby
+# For use with Marked <http://markedapp.com> and Jekyll _posts
+#   turns
+# {% img alignright /images/heythere.jpg 100 100 "Hey there" "hi" %}
+#   into
+# <img src="../images/heythere.jpg" alt="Hey there" class="alignright" title="hi" />
+#
+# replaces alignleft and alignright classes with appropriate style attribute
+# ---
+# Replaces {% gist XXXXX filename.rb %} with appropriate script tag
+#
+# Processes final output with /usr/bin/kramdown (install kramdown as system gem: `sudo gem install kramdown`)
+
+content = STDIN.read
+
+def e_sh(str)
+  str.to_s.gsub(/(?=[^a-zA-Z0-9_.\/\-\x7F-\xFF\n])/, '\\').gsub(/\n/, "'\n'").sub(/^$/, "''")
+end
+
+# Process image Liquid tags
+content.gsub!(/\{% img (.*?) %\}/) {|img|
+  if img =~ /\{% img (\S.*\s+)?(https?:\/\/\S+|\/\S+|\S+\/\s+)(\s+\d+\s+\d+)?(\s+.+)? %\}/i
+    classes = $1.strip if $1
+    src = $2
+    size = $3
+    title = $4
+
+    if /(?:"|')([^"']+)?(?:"|')\s+(?:"|')([^"']+)?(?:"|')/ =~ title
+      title  = $1
+      alt    = $2
+    else
+      alt    = title.gsub!(/"/, '&#34;') if title
+    end
+    classes.gsub!(/"/, '') if classes
+  end
+
+  style = %Q{ style="float:right;margin:0 0 10px 10px"} if classes =~ /alignright/
+  style = %Q{ style="float:left;margin:0 10px 10px 0"} if classes =~ /alignleft/
+
+  %Q{<img src="..#{src}" alt="#{alt}" class="#{classes}" title="#{title}"#{style}>}
+}
+
+# Process gist tags
+content.gsub!(/\{% gist(.*?) %\}/) {|gist|
+    if parts = gist.match(/\{% gist ([\d]*) (.*?)?%\}/)
+      gist_id = parts[1].strip
+      file = parts[2].nil? ? '' : "?file-#{parts[2].strip}"
+      %Q{<script src="https://gist.github.com/#{gist_id}.js#{file}"></script>}
+    else
+      ""
+    end
+}
+
+puts %x{echo #{e_sh content}|/usr/bin/kramdown}
