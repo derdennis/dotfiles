437a13775a6d33aa094370cb63b17714ed688795
Dennis Wegner <dennis@instant-thinking.de>
2013-01-17 10:29:01 +0100

    Renamed the check script, introduced a variable for my home

--- bin/corestick_repo_check.rb
+++ bin/corestick_repo_check.rb
@@ -4,9 +4,9 @@
 # Clear my screen
 puts "\e[H\e[2J"
 
-Dir.glob("/home/dennis/*/.git", File::FNM_DOTMATCH).each do |git_dir|
+Dir.glob("#{ENV['HOME']}/*/.git", File::FNM_DOTMATCH).each do |git_dir|
     git_work_tree = git_dir.gsub(/\.git/, '')
-    git_repo_name = git_work_tree.gsub(/\/home\/dennis\//, '').gsub(/\//, '')
+    git_repo_name = git_work_tree.gsub(/#{ENV['HOME']}/, '').gsub(/\//, '')
 
     build_git_status=`git --git-dir=#{git_dir} --work-tree=#{git_work_tree} remote show corestick 2> /dev/null | tail -n1`.gsub(/\n/,"").strip
     if build_git_status =~ /up to date/
