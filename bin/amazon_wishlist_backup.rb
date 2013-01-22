eded154e971f4f0cf2eefc0c2ef6067c5fd0702c
Dennis Wegner <dennis@instant-thinking.de>
2013-01-22 14:56:22 +0100

    Getting the URLs

--- bin/amazon_wishlist_backup.rb
+++ bin/amazon_wishlist_backup.rb
@@ -34,22 +34,36 @@ puts wishlist.length
 #puts "first item:"
 #puts wishlist[0]
 
-#puts "Title selectors:"
-#puts wishlist[59].css("span.small strong a").text
+puts "Title selectors:"
+puts wishlist[59].css("span.small strong a").text
 
-#puts "Tiny selectors:"
-#puts wishlist[59].css("span.tiny").text.gsub(/Alle Kaufmöglichkeiten$/,'').strip
+puts "URL selectors:"
+urlthings = wishlist[59].css("span.small strong a")
+urlthings.each{|link| puts link['href']}
 
-#puts "Price selectors:"
-#puts wishlist[59].css("span.price").text
+puts "Tiny selectors:"
+puts wishlist[59].css("span.tiny").text.gsub(/Alle Kaufmöglichkeiten$/,'').strip
+
+puts "Price selectors:"
+puts wishlist[59].css("span.price").text
+
+
 puts "List"
 puts ""
 
 wl_index=0
 wishlist.each do |item|
+# Get the title and clean it of some external shop references
 title = item.css("span.small strong a").text.gsub(/in diesem Shop einkaufen/,'')
 
+# Get the URL of each title
+item.css("span.small strong a").each{|link| $amz_url= link['href']}
+#urlthings.each{|link| $amz_url= link['href']}
+puts $amz_url
 
+
+# Get the autor (or whatever it is) and adjust it for books and DVDs. Leave
+# empty if neither "von" nor "DVD" is found.
 autor = item.css("span.tiny").text.gsub(/Alle Kaufmöglichkeiten$/,'').strip
 if autor =~ /von|DVD/
     autor = autor.gsub(/Angeboten von.*?DVD ~/,'DVD ~')
@@ -57,15 +71,15 @@ else
     autor = "BLUB"
 end
 
-
-
+# Get the price
 price = item.css("span.price").text
 
+# Output:
 puts wl_index.to_s + " " + title + " " + autor + " " + price 
+
+# Increase the counter
 wl_index+=1
 
 end
-
-
 
 exit
