29ef3ef3be11c97525f420cee399e4e5c94e68f9
Dennis Wegner <dennis@instant-thinking.de>
2013-06-09 20:49:49 +0200

    Fixed the timestamp format

--- bin/blackbirdpy.py
+++ bin/blackbirdpy.py
@@ -123,7 +123,7 @@ def embed_tweet_html(tweet_url, extra_css=None):
     tweet_text = wrap_entities(tweet).replace('\n', '<br />')
 
     tweet_created_datetime = pytz.utc.localize(tweet.created_at).astimezone(myTZ)
-    tweet_timestamp = tweet_created_datetime.strftime("%a %b %-d %Y %-I:%M %p %Z")
+    tweet_timestamp = tweet_created_datetime.strftime("%a %b %d %Y %H:%M %Z")
 
     if extra_css is None:
         extra_css = {}
