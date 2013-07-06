509d3e798109f85f0639a46a00791d9f3fd4aa3d
Dennis Wegner <dennis@instant-thinking.de>
2013-07-06 08:37:06 +0200

    Added script to refresh fever

---
+++ bin/fever_refresh.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+# Script to get called via launchd every 15 minutes to refresh my fever
+# installation.
+
+echo "Starting to refresh fever..."
+#curl -L -s --user-agent 'Fever Refresh Cron' 'http://fever.instant-thinking.de/?refresh'
+curl -L -s 'http://fever.instant-thinking.de/?refresh'
+echo "Finished refreshing fever."
