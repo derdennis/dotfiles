652d7d92794cb2dc5cd8b3463062cff94524e9a2
Dennis Wegner <dennis@instant-thinking.de>
2015-06-02 15:52:00 +0200

    Removed trailing whitespace

--- bin/is-pdf-ocrd.sh
+++ bin/is-pdf-ocrd.sh
@@ -3,7 +3,7 @@ MYFONTS=$(pdffonts "$1" | tail -n +3 | cut -d' ' -f1 | sort | uniq)
 if [ "$MYFONTS" = '' ] || [ "$MYFONTS" = '[none]' ]; then
     echo "No OCR info in: $1"
     exit 1
-else 
+else
     echo "OCR info found in: $1"
     exit 0
-fi 
+fi
