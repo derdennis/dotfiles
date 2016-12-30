f9b7f0139bf5388ae01aecb4b2c175650de367ca
Dennis Wegner <dennis@instant-thinking.de>
2016-12-30 11:01:29 +0100

    Changed to secure http.

--- bin/it_scripts/fix_htaccess/fix_htaccess.sh
+++ bin/it_scripts/fix_htaccess/fix_htaccess.sh
@@ -3,6 +3,6 @@
 file="/Users/dennis/Sites/new/.htaccess"
 
 sed -i '' 's@\(^RewriteCond %{HTTP_HOST}.*\)@#\1@' $file
-sed -i '' 's@\(^RewriteRule \^(\.\*)$ http://instant-thinking.de.*\)@#\1@' $file    
-sed -i '' 's@\(^# Get rid of www.*\)@#\1 The following two lines get uncommented every five minutes by a script on the mini@' $file    
+sed -i '' 's@\(^RewriteRule \^(\.\*)$ https://instant-thinking.de.*\)@#\1@' $file
+sed -i '' 's@\(^# Get rid of www.*\)@#\1 The following two lines get uncommented every five minutes by a script on the mini@' $file
 
