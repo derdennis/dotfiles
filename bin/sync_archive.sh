14862526d2b7c2e0ec496d106c2e9676805b796a
Dennis Wegner <dennis@instant-thinking.de>
2017-02-18 09:35:06 +0100

    Made the archive sync display an Übersicht widget.

--- bin/sync_archive.sh
+++ bin/sync_archive.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
 
-/usr/bin/rsync -rav --delete-after --out-format "%o  %n" /Users/dennis/Documents/archive/ mini:/Users/dennis/Documents/archive
+date > /var/log/archive_rsync.log
+/usr/bin/rsync -rav --delete-after --out-format "%o  %n" /Users/dennis/Documents/archive/ mini:/Users/dennis/Documents/archive >> /var/log/archive_rsync.log
+
+#/usr/bin/osascript -e 'display notification "Synced the archive to the mini." with title "Archive Sync"'
